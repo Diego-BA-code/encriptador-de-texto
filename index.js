2a9683e6019db62c0868f5bff0f6c422120d6222
Diego-BA-code <diego23272@gmail.com>
2024-03-23 22:17:06 -0600

    La funcion de verificar caracteres ahora admite espacios y verifica si no ingreso un texto, tambien se agrego la funcion para copiar el resultado de encriptacion o desencriptacion

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const avisoEntradas = document.getElementById('aviso-entradas');
 const avisoSinContenido = document.getElementById('aviso-sin-contenido');
 const avisoResultado = document.getElementById('aviso-resultado');
-const campoResultado = document.getElementById('resultado');
+const cajaResultado = document.getElementById('resultado');
 const mensaje = document.getElementById('mensaje');
+const avisoCopiar = document.getElementById('aviso-copiado');
 
-function verificarCaracteres(texto, resultado){
-    if (!/^[a-z]+$/.test(texto)) {
+function verificarCaracteres(texto, resultado) {
+    if (texto === "") {
+        avisoResultado.style.display = 'none';
+        avisoSinContenido.style.display = 'flex';
+        // Si no hay contenido en el input se muestra un aviso
+        avisoSinContenido.style.transition = "opacity 1s";
+        avisoSinContenido.style.opacity = "0";
+        setTimeout(function mostrarAviso() {
+            avisoSinContenido.style.transition = "opacity 0s"
+            avisoSinContenido.style.opacity = "1";
+        }, 500);
+        return;
+    }
+    if (!/^[a-z\s]+$/.test(texto)) {
         // Si tiene mayúsculas se cambia el background del aviso a rojo
         avisoEntradas.style.backgroundColor = "red";
         avisoEntradas.style.transition = "1s";
@@ -17,8 +30,7 @@ function verificarCaracteres(texto, resultado){
     } else {
         avisoSinContenido.style.display = "none";
         avisoResultado.style.display = "flex";
-        mensaje.value = "";
-        campoResultado.value = resultado;
+        cajaResultado.value = resultado;
     }
 }
 function encriptar() {
@@ -36,3 +48,21 @@ function desencriptar() {
     let resultado = textoEncriptado.replace(/imes/g, 'i').replace(/u/g, 'ufat').replace(/enter/g, 'e').replace(/ai/g, 'a').replace(/ober/g, 'o');
     verificarCaracteres(textoEncriptado, resultado);
 }
+
+function copiarTexto() {
+    let avisoCopiado = '¡Texto copiado con éxito!';
+        //seleccionar el texto de la caja
+        cajaResultado.select();
+        cajaResultado.setSelectionRange(0, 999);
+        //copiar el texto de la caja
+        navigator.clipboard.writeText(cajaResultado.value);
+        //avisar que ha copiado el texto
+        avisoCopiar.style.display = 'block'
+        avisoCopiar.style.color = '#000FFF';
+        avisoCopiar.innerText = avisoCopiado;
+        //despues de 2 segundos el aviso desaparece
+        setTimeout(function avisos(){
+
+            avisoCopiar.style.display = 'none';
+            }, 2000);
+    }

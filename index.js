04330b7204ac465492e02f76b01e949d2214d6eb
Diego-BA-code <diego23272@gmail.com>
2024-03-24 03:43:23 -0600

    se modifica el algoritmo de encriptacion de index.js para que funcione en base a los requerimientos

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const avisoEntradas = document.getElementById('aviso-entradas');
-const avisoSinContenido = document.getElementById('aviso-sin-contenido');
-const avisoResultado = document.getElementById('aviso-resultado');
-const cajaResultado = document.getElementById('resultado');
-const mensaje = document.getElementById('mensaje');
-const avisoCopiar = document.getElementById('aviso-copiado');
+const avisoEntradas = document.getElementById("aviso-entradas");
+const avisoSinContenido = document.getElementById("aviso-sin-contenido");
+const avisoResultado = document.getElementById("aviso-resultado");
+const cajaResultado = document.getElementById("resultado");
+const mensaje = document.getElementById("mensaje");
+const avisoCopiar = document.getElementById("aviso-copiado");
 
 function verificarCaracteres(texto, resultado) {
     if (texto === "") {
-        avisoResultado.style.display = 'none';
-        avisoSinContenido.style.display = 'flex';
+        avisoResultado.style.display = "none";
+        avisoSinContenido.style.display = "flex";
         // Si no hay contenido en el input se muestra un aviso
         avisoSinContenido.style.transition = "opacity 1s";
         avisoSinContenido.style.opacity = "0";
@@ -30,6 +30,7 @@ function verificarCaracteres(texto, resultado) {
     } else {
         avisoSinContenido.style.display = "none";
         avisoResultado.style.display = "flex";
+        mensaje.value = ""
         cajaResultado.value = resultado;
     }
 }
@@ -37,57 +38,107 @@ function encriptar() {
     // Obtener el valor de entrada
     let textoSinEncriptar = mensaje.value;
     // Remplazar ciertos caracteres del mensaje
-    let resultado = textoSinEncriptar;
-
-    const remplazos = [
-        { search: 'a', replace: 'ai' },
-        { search: 'e', replace: 'enter' },
-        { search: 'i', replace: 'imes' },
-        { search: 'o', replace: 'ober' },
-        { search: 'u', replace: 'ufat' }
-    ];
-
-    for (let i = 0; i < remplazos.length; i++) {
-        resultado = resultado.replace(new RegExp(remplazos[i].search, 'g'), remplazos[i].replace);
+    let resultado = "";
+    // iterar por cada caracter del textoSinEncriptar
+    for (i = 0; i < textoSinEncriptar.length; i++) {
+        resultado += enc(textoSinEncriptar[i]);
     }
-
     verificarCaracteres(textoSinEncriptar, resultado);
 }
 
 function desencriptar() {
     //obtener el valor del texto encriptado
     let textoEncriptado = mensaje.value;
-    let resultado = textoEncriptado;
+    let resultado = "";
 
-    const remplazos = [
-        { search: 'ai', replace: 'a' },
-        { search: 'enter', replace: 'e' },
-        { search: 'imes', replace: 'i' },
-        { search: 'ober', replace: 'o' },
-        { search: 'ufat', replace: 'u' }
-    ];
-
-    for (let i = 0; i < remplazos.length; i++) {
-        resultado = resultado.replace(new RegExp(remplazos[i].search, 'g'), remplazos[i].replace);
+    // Dividir el texto encriptado en las partes correspondientes
+    let partes = textoEncriptado.split(/(ai|enter|imes|ober|ufat|\s)/);
+    //iterar por cada parte del textoEncriptado
+    for (i = 0; i < partes.length; i++) {
+        resultado += des(partes[i]);
     }
 
     verificarCaracteres(textoEncriptado, resultado);
 }
 
-function copiarTexto() {
-    let avisoCopiado = '¡Texto copiado con éxito!';
-        //seleccionar el texto de la caja
-        cajaResultado.select();
-        cajaResultado.setSelectionRange(0, 999);
-        //copiar el texto de la caja
-        navigator.clipboard.writeText(cajaResultado.value);
-        //avisar que ha copiado el texto
-        avisoCopiar.style.display = 'block'
-        avisoCopiar.style.color = '#000FFF';
-        avisoCopiar.innerText = avisoCopiado;
-        //despues de 2 segundos el aviso desaparece
-        setTimeout(function avisos(){
+function des(x) {
+    switch (x) {
+        case " ":
+            return " ";
+            break;
 
-            avisoCopiar.style.display = 'none';
-            }, 2000);
+        case "ai":
+            return "a";
+            break;
+
+        case "enter":
+            return "e";
+            break;
+
+        case "imes":
+            return "i";
+            break;
+
+        case "ober":
+            return "o";
+            break;
+
+        case "ufat":
+            return "u";
+            break;
+
+        default:
+            return x;
+            break;
     }
+}
+
+function enc(x) {
+    switch (x) {
+        case " ":
+            return " ";
+            break;
+
+        case "a":
+            return "ai";
+            break;
+
+        case "e":
+            return "enter";
+            break;
+
+        case "i":
+            return "imes"
+            break;
+
+        case "o":
+            return "ober"
+            break;
+
+        case "u":
+            return "ufat"
+            break;
+
+        default:
+            return x;
+            break;
+    }
+}
+
+function copiarTexto() {
+    let avisoCopiado = "¡Texto copiado con éxito!";
+    //seleccionar el texto de la caja
+    cajaResultado.select();
+    cajaResultado.setSelectionRange(0, 999);
+    //copiar el texto de la caja
+    navigator.clipboard.writeText(cajaResultado.value);
+    //avisar que ha copiado el texto
+    avisoCopiar.style.display = "block"
+    avisoCopiar.style.color = "#000FFF";
+    avisoCopiar.innerText = avisoCopiado;
+    //despues de 2 segundos el aviso desaparece
+    setTimeout(function avisos() {
+
+        avisoCopiar.style.display = "none";
+    }, 2000);
+}
